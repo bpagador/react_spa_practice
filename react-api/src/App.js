9b359c33fc7af8b771e283dc9881a6010fbe2e08
Briseida Pagador <bpagador@gmail.com>
2020-11-22 19:06:54 -0800

    rendered from sample api

--- react-api/src/App.js
+++ react-api/src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import Contacts from "./Contacts";
 
 class App extends Component {
   state = {
     contacts: []
   }
 
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ contacts: data})
+    })
+    .catch(console.log)
+  }
+
 render() {
   return (
-    <div className="card">
-    <div class="card-body">
-      <h5 class="card-title">Steve Jobs</h5>
-      <h6 class="card-subtitle mb-2 text-muted">steve@apple.com</h6>
-      <p class="card-text">Stay Hungry, Stay Foolish</p>
-    </div>
-  </div>
+    <Contacts contacts={this.state.contacts} />
   );
 }
   
---
+++ react-api/src/Contacts.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Contacts = ({ contacts }) =>  {
+    return (
+        <div>
+            <h1>Contact List</h1>
+            {contacts.map((contact) => (
+                <div className="card">
+                    <section className="card-body">
+                        <h1 className="card-title">{contact.name}</h1>
+                        <h2 className="card-email">{contact.email}</h2>
+                        <h3 className="card-website">{contact.website}</h3>
+                    </section>
+                </div>
+            ))}
+            
+        </div>
+    )
+}
+export default Contacts;
