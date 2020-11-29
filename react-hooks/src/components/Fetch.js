6528cd5e8e988c47472d8b1ae93ccecb3f9d4725
Briseida Pagador <bpagador@gmail.com>
2020-11-29 10:30:35 -0800

    fixed quick typo

--- react-hooks/src/components/Fetch.js
+++ react-hooks/src/components/Fetch.js
@@ -1,18 +1,22 @@
+import React from 'react'
 import  { useState, useEffect } from 'react'
 
 export default function Fetch() {
     //state
-    const [ data, setData ] = useState({});
+    const [ data, setData ] = useState([]);
 
     //lifecyle method 
     useEffect(() => {
-        fetch("http://jsonplaceholder.typicode.com/posts")
+        fetch("http://jsonplaceholder.typicode.com/posts/")
             .then(res => res.json())
             .then(data => setData(data))
+            .catch(console.log)
     }, [])
 
     return (
-        <div>
+        
+        <div> 
+            <h2>Hi</h2>
             <ul>
                 {data.map(el => (
                     <li key={el.id}>{el.title}</li>
