ed781c87474a4c83e5c05b4c51c20a02b68c4a74
Briseida Pagador <bpagador@gmail.com>
2020-11-29 19:30:53 -0800

    fetch.js not rendering

--- react-hooks/src/components/Fetch.js
+++ react-hooks/src/components/Fetch.js
@@ -1,25 +1,35 @@
 import React from 'react'
 import  { useState, useEffect } from 'react'
 
-export default function Fetch() {
-    //state
-    const [ data, setData ] = useState([]);
 
-    //lifecyle method 
+function usePosts() {
+    const [posts, setPosts] = useState({});
+
     useEffect(() => {
-        fetch("http://jsonplaceholder.typicode.com/posts/")
-            .then(res => res.json())
-            .then(data => setData(data))
-            .catch(console.log)
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then(res => res.json())
+        .then(data => {
+            setPosts(data)
+        })
     }, [])
 
+    return posts; 
+}
+
+export default function Fetch() {
+    const posts = usePosts()
+
+
     return (
-        
+  
         <div> 
             <h2>Hi</h2>
             <ul>
-                {data.map(el => (
-                    <li key={el.id}>{el.title}</li>
+                {posts.map(item => (
+                    <li key={item.id}>
+                        <h2>{item.title}</h2>
+                        
+                    </li>
                 ))}
             </ul>
         </div>
